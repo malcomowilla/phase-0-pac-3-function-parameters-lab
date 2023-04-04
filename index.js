2635d73c3ae880e9ee1ad8141fcd03ffb57b25e2
malcomowilla <owillamalcom@gmail.com>
2023-04-04 12:08:49 +0300

    solved the problem and it feels good

--- index.js
+++ index.js
@@ -1,10 +1,109 @@
-function introduction(name){
-return "Hi, my name is Samip ${name}." 
-return "Hi, my name is Aki ${name}."   
+// function introduction(name){
+// return "Hi, my name is Samip ${name}." 
+// return "Hi, my name is Aki ${name}."   
+// }
+//   function introductionWithLanguage(name, language){
+//     return "Hi my name is" + name +" and I am learning to program in" +language 
+//   }  
+// function introductionWithLanguageOptional(name, language = "javascript"){
+//     return "Hi my name is " + name + " and I am learning to program in" + language
+
+// }
+
+// function logTwoValues(value1, value2) {
+//   console.log(`The two values are ${value1} and ${value2}.`);
+
+// }
+
+
+// function sayHelloTo(firstName = "User") {
+//   console.log(`Hello, ${firstName}!`)
+// }
+
+// sayHelloTo()
+
+
+function introduction(name) {
+  return `Hi, my name is ${name}.`
 }
-  function introductionWithLanguage(name, language){
-    return "Hi my name is" + name +" and I am learning to program in" +language 
-  }  
-function introductionWithLanguageOptional(name, language = "javascript"){
-    return "Hi my name is " + name + " and I am learning to program in" + language
+
+
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  
+
+  
 }
+
+
+function introductionWithLanguageOptional(name ="Gracie", language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
